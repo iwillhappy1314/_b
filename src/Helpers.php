1cf4161f99ee0af66992739eabfa228621461fa1
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-04-13 17:04:20 +0800

    Path fix

--- src/Helpers.php
+++ src/Helpers.php
@@ -16,12 +16,12 @@ class Helpers
      */
     public static function get_assets_url($name, $asset, $directory = 'dist')
     {
-        $filepath = realpath(SPACENAME_PATH . '/' . $directory . '/' . $name . '/manifest.json');
+        $filepath = realpath(SPACENAME_PATH . $directory . '/' . $name . '/manifest.json');
 
         if (file_exists($filepath)) {
             $assets = json_decode(file_get_contents($filepath), true);
 
-            return esc_url(SPACENAME_URL . '/' . $directory . '/' . $assets[ $asset ]);
+            return esc_url(SPACENAME_URL . $directory . '/' . $assets[ $asset ]);
         }
 
         return false;
