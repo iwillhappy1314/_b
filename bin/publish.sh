b24817d5505e7e4e84aaf9e9efa4ae1d5e9a3a62
Amos Lee <iwillhappy1314@gmail.com>
2023-05-25 18:16:17 +0800

    update publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,7 +8,7 @@ if [ -f $zip_name ]; then
 fi
 
 if [ $1 = "production" ]; then
-    zip -r $zip_name . -x "*.yaml" -x "*.lock" -x "*.json" -x "*.js" -x "bin/*" -x ".git/*" -x "node_modules/*"
+    zip -r $zip_name . -x "frontent/*.yaml" -x "frontent/*.lock" -x "frontent/*.json" -x "frontent/*.js" -x "bin/*" -x ".git/*" -x "frontent/node_modules/*"
 else
-    zip -r $zip_name . -x ".git/*" -x "node_modules/*"
+    zip -r $zip_name . -x ".git/*" -x "frontent/node_modules/*"
 fi
