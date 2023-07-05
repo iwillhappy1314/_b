ae5bac46c01c1a11be4c97ed750e9e067c0cb7a0
Amos Lee <iwillhappy1314@gmail.com>
2023-07-05 18:19:43 +0800

    Add release ignore file

---
+++ release.config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'bin/',
+    'composer.json',
+    'composer.lock',
+    '.gitignore',
+    'frontend/.jshintrc',
+    'frontend/package.json',
+    'frontend/pnpm-lock.yaml',
+    'frontend/postcss.config.js',
+    'frontend/tailwind.config.js',
+    'frontend/webpack.mix.js',
+    'frontend/webpack.mix.js',
+    'frontend/release.config.php',
+];
